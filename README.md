f769645fcfd5f302bff2bb796f7c907cc95c7ad8
Fabio de Matos <fmatosqg@gmail.com>
2020-06-29 16:17:34 +1000

    Use Android icons and standards

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ IOS/Android Reddit app created with Flutter
 
 ## Contribution
 
-I'm open to anyone contributing to this repo. I'd advise using Android Studio and running a `dartfmt` before creating a pull request.
+I'm open to anyone contributing to this repo. I'd advise using Android Studio and running a `dartfmt` before creating a pull request: `dartfmt -w lib`.
 Pages are setup using [Mobx](https://pub.dev/packages/mobx) and a viewmodel pattern. Check out `src/main.dart` and `src/mainpage_viewmodel.dart`.
 
 ## Images
--- lib/src/common/PostIcon.dart
+++ lib/src/common/PostIcon.dart
@@ -1,19 +1,25 @@
+import 'dart:io';
+
 import 'package:draw/draw.dart';
 import 'package:flutter/material.dart';
 
 class SubredditPost {
-
   static Widget buildPostIcon(Submission submission) {
     if (submission.isSelf) {
-      return _buildIcon(Icons.arrow_forward_ios);
+      return _buildIcon(
+        Platform.isAndroid ? Icons.arrow_right : Icons.arrow_forward,
+      );
     } else if (submission.isVideo) {
       return _buildIcon(Icons.videocam);
     } else {
-      return _buildIcon(Icons.launch);
+      return _buildIcon(  Platform.isAndroid ? Icons.open_in_browser: Icons.launch);
     }
   }
 
   static Widget _buildIcon(IconData icon) {
-    return Icon(icon, size: 25,);
+    return Icon(
+      icon,
+      size: 25,
+    );
   }
 }
